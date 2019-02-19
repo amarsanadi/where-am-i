701d6310b3ba1846b02a8717cf9b6236ad9c79cb
Amar Sanadi <amar.sanadi@gmail.com>
2019-02-19 23:24:32 +0000

    feat: implemented rudimentary proportional control.
    Updated the process_image node to more effectively direct the robot towards the ball. The process_image node now calculates an "error" quantity and sends the angle to turn with a factor multiplied by the error.

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -53,7 +53,7 @@ void process_image_callback(const sensor_msgs::Image img)
     {
       ROS_INFO_STREAM("ball in the left 1/3");
       lin_x=0.1;
-      ang_z=0.1;
+      ang_z=1.5+0.01*(400-ball_x);
     } 
     else
       if(ball_x >= 266 && ball_x < 533)
@@ -61,9 +61,9 @@ void process_image_callback(const sensor_msgs::Image img)
         ROS_INFO_STREAM("ball in the middle section");
         lin_x=0.4;
         if(ball_x<400)
-          ang_z=0.05;
+          ang_z=0.05+0.01*(400-ball_x);
         else
-          ang_z=-0.05;
+          ang_z=-0.05-0.01*(ball_x-400);
         
       }
       else
@@ -71,7 +71,7 @@ void process_image_callback(const sensor_msgs::Image img)
         {
           ROS_INFO_STREAM("ball in the right section");
           lin_x=0.1;
-          ang_z=-0.1;
+          ang_z=-1.5-0.01*(ball_x-400);
         }
     drive_robot(lin_x, ang_z);
   } else drive_robot(0, -0.5);
